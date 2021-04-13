f647ed77f09934891e36873ffe6ee2721b920e53
duarteicaros <icaroduartec@gmail.com>
2021-04-12 21:53:03 -0300

    Implementação da Classificação

--- index.js
+++ index.js
@@ -19,6 +19,12 @@ const funcoes = {
 
             observacoes;
 
+    },
+    ObservacaoAtualizada: (observacao) => {
+        const observacoes =
+            baseConsulta[observacao.lembreteId]["observacoes"];
+        const indice = observacoes.findIndex((o) => o.id === observacao.id);
+        observacoes[indice] = observacao;
     }
 };
 
@@ -27,7 +33,9 @@ app.get("/lembretes", (req, res) => {
 });
 
 app.post("/eventos", (req, res) => {
-    funcoes[req.body.tipo](req.body.dados);
+    try {
+        funcoes[req.body.tipo](req.body.dados);
+    } catch (err) {}
     res.status(200).send(baseConsulta);
 });
 
